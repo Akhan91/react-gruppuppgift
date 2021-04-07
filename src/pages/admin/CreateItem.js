e44eb2de05d770bfa3cf9b5ebcea209aed434291
Emil Avara <emilavara@hotmail.com>
2021-04-07 14:07:39 +0200

    Init commit

---
+++ src/pages/admin/CreateItem.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import CreatePostForm from '../../components/CreatePostForm';
+
+function CreateItem() {
+    
+
+    return (
+        <CreatePostForm />
+    );
+}
+
+export default CreateItem;
